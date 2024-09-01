347e80249ab3bb31d78fb67f302e2256f3716f20
ganesh-tatkare <gtatkare42@gmail.com>
2024-09-01 20:08:52 +0530

    images api integrated

---
+++ TimeGuesser/src/utility/utility.js
@@ -0,0 +1,8 @@
+// import { createApi } from "unsplash-js";
+
+// export const utility = {
+//     splashApi : createApi({
+//         // Don't forget to set your access token here!
+//         accessKey: import.meta.env.UNSPLASH_IMAGE_API_KEY
+//     }),
+// }
